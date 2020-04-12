c57331c8d043d286516bca914fb4db781b069fd1
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-12 21:30:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here
+function findMatching(drivers, name){
+  return drivers.filter(n => n.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, part){
+  check = RegExp("^"+part)
+  return drivers.filter(n => n.toLowerCase().includes(part.toLowerCase()))
+}
